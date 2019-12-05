631c47909e1fdb79a618a2c20d20173dab06f84b
braha-h <braha-h@142.132.136.210>
2019-12-05 09:48:58 -0600

    Added getters and setters for each property

--- src/Employee.java
+++ src/Employee.java
@@ -72,7 +72,7 @@ public class Employee {  // begin class
         ***********************************************************/
         public int getID(){ // begin getter
             
-            
+            return this.ID;                                                     // return employee's ID
             
         } // end getter
         
@@ -85,6 +85,11 @@ public class Employee {  // begin class
         * 
         * @return   hours: int
         ***********************************************************/
+        public int getHours(){ // begin getter
+            
+            return this.hours;                                                  // return hours worked
+            
+        } // end getter
         
         /***********************************************************
         * Description: Get employee's hourly wage
@@ -95,7 +100,42 @@ public class Employee {  // begin class
         * 
         * @return   wage: double
         ***********************************************************/
+        public double getWage(){ // begin getter
+            
+            return this.wage;                                                   // return hourly wage
+            
+        } // end getter
 	    	
     // ********** Setters **********
+        
+         /***********************************************************
+        * Description: Set a new value for employee's hours worked
+        * 
+        * Interface:
+        * 
+        * @param    h: hours
+        * 
+        * @return
+        ***********************************************************/
+        public void setWage(int h){ // begin setter
+            
+            hours = h;                                                          // set new hours worked
+            
+        } // end setter
+        
+        /***********************************************************
+        * Description: Set a new value for employee's hourly wage
+        * 
+        * Interface:
+        * 
+        * @param    w: wage
+        * 
+        * @return
+        ***********************************************************/
+        public void setWage(double w){ // begin setter
+            
+            wage = w;                                                           // set new wage
+            
+        } // end setter
 
 }  // end class
